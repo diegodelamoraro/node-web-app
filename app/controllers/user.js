5b341f1e03710dbd7b60f23faa0d1ec2c8bcac8d
diegodelamoraro <102392702+diegodelamoraro@users.noreply.github.com>
2022-06-19 11:06:23 -0500

    API finalizada

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -9,10 +9,11 @@ const options = {
 const parseId = (id) => {
   return mongoose.Types.ObjectId(id);
 };
+
 /**
  * Obtener DATA de USUARIOS
+ * Ejemplo: http://0.0.0.0:8080/user/
  */
-
 exports.getUsers = (req, res) => {
   model.paginate({}, options, (err, docs) => {
     res.send({
@@ -23,8 +24,8 @@ exports.getUsers = (req, res) => {
 
 /**
  * Obtener DATA de USUARIOS
+ * Ejemplo: http://0.0.0.0:8080/user/62af2dfa32e701371611dcce
  */
-
 exports.getUser = (req, res) => {
   model.findOne({ _id: parseId(req.params.id) }, (err, docs) => {
     res.send({
@@ -34,16 +35,45 @@ exports.getUser = (req, res) => {
 };
 
 /**
- * Obtener DATA de USUARIOS
+ * Obtener DATA de USUARIOS pot type
+ * Ejemplo: http://0.0.0.0:8080/user/type/admin/department/gerencia
  */
+exports.getUsersBy = (req, res) => {
+  console.log("getUsersBy params => ", req.params);
+  model.find(
+    {
+      $and: [{ type: req.params.type }, { department: req.params.department }],
+    },
+    (err, docs) => {
+      res.send({
+        items: docs,
+      });
+    }
+  );
+};
 
+/**
+ * Actualizar DATA de USUARIOS por id
+ */
 exports.updateUser = (req, res) => {
-  const { id } = req.params;
+  const id = parseId(req.params.id);
   const body = req.body;
-  model.updateOne({ _id: parseId(id) }, body, (err, docs) => {
-    res.send({
-      items: docs,
-    });
+  console.log("id => ", id, "body =>", body);
+  model.findOne({ _id: id }, (err, docs) => {
+    // console.log("findOne err =>", err, "docs =>", docs);
+    if (docs == null) {
+      model.create(body, (err, docs) => {
+        // console.log("create err =>", err, "docs =>", docs);
+        if (err) res.status(422).send({ error: "Error" });
+        else res.status(201).send(docs);
+      });
+    } else {
+      model.updateOne({ _id: id }, body, (err, docs) => {
+        // console.log("updateOne err =>", err, "docs =>", docs);
+        if (err) res.status(422).send({ error: "Error" });
+        else res.status(200).send(docs);
+      });
+    }
   });
 };
 
@@ -64,12 +94,15 @@ exports.insertUser = (req, res) => {
 /**
  * Obtener DATA de USUARIOS
  */
-
-exports.deleteUser = (req, res) => {
-  const { id } = req.params;
-  model.deleteOne({ _id: parseId(id) }, (err, docs) => {
-    res.send({
-      items: docs,
-    });
+exports.deleteUsers = (req, res) => {
+  const { department } = req.params;
+  model.deleteMany({ department }, (err, docs) => {
+    console.log("deleteMany err =>", err, "docs =>", docs);
+    if (err) res.status(422).send({ error: "Error" });
+    else {
+      if (docs == null) res.sendStatus(204);
+      else if (docs.deletedCount > 0) res.status(200).send(docs);
+      else res.sendStatus(204);
+    }
   });
 };
